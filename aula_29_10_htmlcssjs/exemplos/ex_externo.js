e46a25c0c3aff27ff7d517e9cec190a4ce0d2ea5
cauafsantosdev <cauafsantosdev@gmail.com>
2024-10-29 11:31:11 -0300

    commiting exercises

---
+++ aula_29_10_htmlcssjs/exemplos/ex_externo.js
@@ -0,0 +1,8 @@
+function funcaoHello() {
+    alert("Hello World!");
+}
+
+function constante(){
+    const valor = 10;
+    document.write(`O valor constante é ${valor}`);
+}
---
+++ aula_29_10_htmlcssjs/exercicio/ex1.js
@@ -0,0 +1,28 @@
+function getSum() {
+    var x = document.getElementById("x");
+    var y = document.getElementById("y");
+    var sum = parseInt(x.value) + parseInt(y.value);
+    alert(sum);
+}
+
+function getSub() {
+    var x = document.getElementById("x");
+    var y = document.getElementById("y");
+    var sub = parseInt(x.value) - parseInt(y.value);
+    alert(sub);
+}
+
+function getMult() {
+    var x = document.getElementById("x");
+    var y = document.getElementById("y");
+    var mult = parseInt(x.value) * parseInt(y.value);
+    alert(mult);
+}
+
+function getDiv() {
+    var x = document.getElementById("x");
+    var y = document.getElementById("y");
+    var div = parseInt(x.value) / parseInt(y.value);
+    alert(div);
+}
+
